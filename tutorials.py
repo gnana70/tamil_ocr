c89012303a65ef649a685d74d9d31462a735a3ec
DocumentAIParser <gnana70@gmail.com>
2024-01-27 18:23:36 +0530

    cuda issue resolution

--- tutorials.py
+++ tutorials.py
@@ -1,10 +1,10 @@
 from ocr_tamil.ocr import OCR
 
 image_path = r"C:\Users\gnana\Documents\GitHub\tamil_ocr\test_images\1.jpg" # insert your own path here
-model_path = r"C:\Users\gnana\Documents\GitHub\tamil_ocr\tamil_ocr\model_weights\parseq_tamil_v6.ckpt"
+model_path = r"C:\Users\gnana\Documents\GitHub\tamil_ocr\ocr_tamil\model_weights\parseq_tamil_v6.ckpt"
 
 
-ocr = OCR(tamil_model_path=model_path)
+ocr = OCR(tamil_model_path=model_path,enable_cuda=False)
 texts = ocr.predict(image_path)
 
 print(texts)
