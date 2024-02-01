5b8f48b2329aad3ac7335eb4dba66d1e42d10d9d
DocumentAIParser <gnana70@gmail.com>
2024-02-01 21:41:52 +0530

    updated code and requirements.txt

--- tutorials.py
+++ tutorials.py
@@ -1,6 +1,6 @@
 from ocr_tamil.ocr import OCR
 
-image_path = r"test_images\english_1.png" # insert your own path here
+image_path = r"test_images\0.jpg" # insert your own path here
 ocr = OCR(detect=True,enable_cuda=False)
 texts = ocr.predict(image_path)
 
