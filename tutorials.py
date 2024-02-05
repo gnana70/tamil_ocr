2c1b6313edcc9343953be0d2058c6c423a7a2f09
DocumentAIParser <gnana70@gmail.com>
2024-02-05 23:36:03 +0530

    readme update

--- tutorials.py
+++ tutorials.py
@@ -1,7 +1,7 @@
 from ocr_tamil.ocr import OCR
 
-image_path = r"test_images\1.jpg" # insert your own path here
-ocr = OCR(detect=False,enable_cuda=False)
+image_path = r"test_images\tamil_sentence.jpg" # insert your own path here
+ocr = OCR(detect=True,enable_cuda=False)
 texts = ocr.predict(image_path,save_image=True)
 
 with open("output.txt","w",encoding="utf-8") as f:
